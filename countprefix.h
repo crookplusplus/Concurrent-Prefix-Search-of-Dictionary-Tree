56f649bd35f4dcd60bd791151387dca0e80f00e0
Chris Rook <101030498+crookplusplus@users.noreply.github.com>
2023-09-12 16:59:42 -0700

    Header for prefix counter thread

---
+++ countprefix.h
@@ -0,0 +1,13 @@
+/*
+*Authored by: Chris Rook
+*
+*/
+
+#ifndef COUNTPREFIX_H
+#define COUNTPREFIX_H
+
+///@brief Method for thread that counts prefixes that have been searched in the tree
+// and outputs them into a countprefix_output.txt file
+void* countprefix(void *);
+
+#endif
